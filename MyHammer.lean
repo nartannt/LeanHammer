94d8a191a4c8a9ef071ec0fd2c80e173f870d38f
nartan <bozec.tanguy@gmail.com>
2026-06-09 14:42:10 +0200

    updated gitignore and reproducing bug

--- MyHammer.lean
+++ MyHammer.lean
@@ -3,3 +3,8 @@ import MyHammer.DuperCore
 import MyHammer.HammerCore
 import MyHammer.Options
 import MyHammer.SingleRuleTac
+
+set_option trace.auto.tptp.printQuery true
+set_option trace.auto.tptp.result true
+example : forall x : Nat, False := by
+  hammer [exists_true_left] {disableAesop := true, autoPremises := 0}
