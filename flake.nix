2907fc56e85e00c6020fc289df8a3f7bbee4c48a
nartan <bozec.tanguy@gmail.com>
2026-06-10 15:36:00 +0200

    removed isabell from flake

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       in rec {
          devShells.default = pkgs.mkShell {
           name = "hammer_time-dev";
-          packages = with pkgs; [ lean4 elan isabelle ];
+          packages = with pkgs; [ lean4 elan ];
        };
 
     });
