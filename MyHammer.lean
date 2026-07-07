69499a4b8a1b50b94d5bd921b6404461f50ad4b3
nartan <bozec.tanguy@gmail.com>
2026-07-07 14:41:33 +0200

    commented out the example

--- MyHammer.lean
+++ MyHammer.lean
@@ -5,22 +5,22 @@ import MyHammer.Options
 import MyHammer.SingleRuleTac
 import MyHammer.MkIffLemmas
 
-set_option trace.auto.tptp.printQuery true
-set_option trace.auto.tptp.result true
-set_option hammer.preprocessingDefault "no_preprocessing"
-set_option hammer.disableAesopDefault true
-set_option hammer.autoPremisesDefault 16
-set_option trace.hammer.premises true
-set_option trace.debug true
-set_option pp.rawOnError true
-
-open Lean LibrarySuggestions in
-set_library_suggestions mepoSelector (useRarity := false)
-
-inductive A : Type
-
-inductive B : A -> Prop where
-  | b x : B x
-
-example : forall x: A, B x := by
-  myhammer [] {autoPremises := 16, disableAesop:= true}
+--set_option trace.auto.tptp.printQuery true
+--set_option trace.auto.tptp.result true
+--set_option hammer.preprocessingDefault "no_preprocessing"
+--set_option hammer.disableAesopDefault true
+--set_option hammer.autoPremisesDefault 16
+--set_option trace.hammer.premises true
+--set_option trace.debug true
+--set_option pp.rawOnError true
+--
+--open Lean LibrarySuggestions in
+--set_library_suggestions mepoSelector (useRarity := false)
+--
+--inductive A : Type
+--
+--inductive B : A -> Prop where
+--  | b x : B x
+--
+--example : forall x: A, B x := by
+--  myhammer [] {autoPremises := 16, disableAesop:= true}
